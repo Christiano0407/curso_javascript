a1e3e8fc852455b4a0626b6a0c13df4dbca6b443
Christiano0407 <placebo0704@gmail.com>
2020-10-20 09:59:45 -0500

    ciclos

---
+++ apps.js
@@ -0,0 +1,16 @@
+/* ARRAY (eSTRUCTURA DE DATOS) */
+
+//Forma de generar un estructura de datos.
+//[0] Posición de comienzo para ingresar.
+
+//Métodos (Mutar o cambiar o agregar)
+//.length(Cuántos hay?).
+//.push(Añadir elementos al final del array).
+//.pop(eliminar el último elemento en el array).
+//.unshift(agrgar hasta el inicio).
+//.shift(eliminar elemento que está en el inicio).
+//.indexOf(La posición de los elementos).
+
+/* LOOPS / FOR / FOR OF (CICLOS) */
+
+//sON Ciclos
