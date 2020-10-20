8127582ad4f78873c0924918eb4278373dfe083d
Christiano0407 <placebo0704@gmail.com>
2020-10-19 23:43:19 -0500

    scoopes

---
+++ app.js
@@ -0,0 +1,32 @@
+/* var nombre = "Luisa";
+
+var elementos = ["Compu", "watch", "mouse"]; */
+
+//funciones
+// 1) declarativa
+/* function miFunction() {
+    console.log("Hola!");
+}
+miFunction(); */
+
+//2)Expresión
+/* var miFunction = function() {
+    console.log("Ya");
+};
+miFunction(); */
+
+//parámetro (parámetro) = No tiene un valor definido = Lo mandas llamar en la función
+/* a) */
+/* function miDesayuno (fruta) {
+    console.log(`Mi desayuno es una ${fruta}`);
+}
+miDesayuno("Manzana"); */
+
+/* b) */
+/* function sumar(a,b) {
+    var resultado = a + b;
+    return resultado;
+}
+sumar(15, 5); */
+
+/* SCOOPE (VAR/LET/CONST) */
