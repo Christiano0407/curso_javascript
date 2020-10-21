3be450f375cbeb1b129f2e3c13bf12c1bca1dd36
Christiano0407 <placebo0704@gmail.com>
2020-10-21 08:38:49 -0500

    one-js

---
+++ main.js
@@ -0,0 +1,9 @@
+// Variables (Declarar / Asignar / Ejecutar).
+/* const text = document.querySelector(`p`);
+text.addEventListener(`click`,updateName );
+
+function updateName() {
+    let name = prompt(`Enter the new Name`);
+    text.textContent = `player 1:` + " " + name;
+} */
+
