86048afa64db31833ef71b299a733cf10c9c5c21
Christiano0407 <placebo0704@gmail.com>
2020-10-21 09:03:38 -0500

    strings

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ function updateName() {
     text.textContent = `player 1:` + " " + name;
 } */
 
+/* STRING AND VARIABLES */
+/* var name = "Chris";
+var apellido = "Velázquez"; */
+//String = Lo que podemos hacer con el.
+/* let nombreMayor = name.toUpperCase();
+let apellidoMenor = apellido.toLowerCase(); */
+//Acceder a un caracter
+/* let primeraLetra = name.charAt(0); */
+//Cantidad de letras o elementos que lo componen. Es un atributo porque no lo imbocamos con un ().
+/* let cantidadLetras = apellido.length */
+// Concatenar = + " " + / Interpolación de variables y texto = `${código js}`.
+/* let nombreCompleto = `${name} ${apellido.toUpperCase()}` */
+//subString
+/* let str = name.substr(1, 3); */
