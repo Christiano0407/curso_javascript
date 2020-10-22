d440eddd78db369bdd5ca78102802e14a07282d6
Christiano0407 <placebo0704@gmail.com>
2020-10-22 08:17:57 -0500

    Prototype

--- object.js
+++ object.js
@@ -8,7 +8,23 @@
 /* CLASS */
 //No es un objeto, es un modelo que define las características que un objeto debería tener.
 //instancias de objetos = datos y funcionalidades. 
-
-
+// new = crear nuevos objetos o prototipos. 
+/* Prototype */
+//agregar una nueva función o constructor / objeto.
+//Los objetos heredan características entre si.
+//Puedes añadir métodos, o nuevas propiedas.
 
 /* CONSTRUCTORES / INSTANCIAS DE OBJETOS / PROTOTYPE*/
+//1)Ejercicio
+/* function persona(nombre, apellido) {
+ this.nombre = nombre;
+ this.apellido = apellido;
+}
+persona.prototype.saludar = function() {
+    console.log(`Hola!, soy ${this.nombre}, ${this.apellido}`)
+}
+
+let personas = new persona("Kader", "Velázquez");
+let erika  = new persona("Erika", "Luna");
+personas.saludar();
+erika.saludar(); */
