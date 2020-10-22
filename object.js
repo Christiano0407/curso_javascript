16da18ddc4825f7bf10d50547659c450cae8061f
Christiano0407 <placebo0704@gmail.com>
2020-10-21 22:35:47 -0500

    POO

---
+++ object.js
@@ -0,0 +1,14 @@
+/* PROGRAMACIÓN ORIENTADA A OBJETOS (POO) */
+//classes
+//functions constructoras
+//objetos y instancias del objeto.
+//Usamos Objetos para modelar cosas del mundo real que queremos representar.
+//Los objetos usan almacenamiento de datos que se envían por la red (API). 
+
+/* CLASS */
+//No es un objeto, es un modelo que define las características que un objeto debería tener.
+//instancias de objetos = datos y funcionalidades. 
+
+
+
+/* CONSTRUCTORES / INSTANCIAS DE OBJETOS / PROTOTYPE*/
