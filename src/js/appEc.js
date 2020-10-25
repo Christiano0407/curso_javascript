1c1bc39a2d74e7f4cdbcb1a9115b1060e07d4d7a
Christiano0407 <placebo0704@gmail.com>
2020-10-25 10:37:10 -0600

    ESC6

---
+++ src/js/appEc.js
@@ -0,0 +1,20 @@
+//ES6 
+/* function newFunction(name = "Kader", age = "24", country = "CDMX"){
+ if(name === "Kader") {
+    console.log("Ella es la mujer más hermosa")
+ }else {
+  console.log("No!")
+ }
+ console.log(name,age, country);
+}
+newFunction(); */
+/* newFunction(`Luisa`, 29, `LO`); */
+
+// Concatenar template `` = comillas francesas.
+/* let epic1 = "Hello";
+let epic2 = "girl";
+let epicPhrase = `${epic1} yo soy ${epic2}`;
+console.log(epicPhrase); */
+
+//Multilínea, Spread, Destructurar, LET Y CONST
+
