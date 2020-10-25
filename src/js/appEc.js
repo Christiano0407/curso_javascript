8ffc977ebf7fd01dabb7b8332d4829898cdbe9da
Christiano0407 <placebo0704@gmail.com>
2020-10-25 11:18:39 -0600

    let

--- src/js/appEc.js
+++ src/js/appEc.js
@@ -17,4 +17,43 @@ let epicPhrase = `${epic1} yo soy ${epic2}`;
 console.log(epicPhrase); */
 
 //Multilínea, Spread, Destructurar, LET Y CONST
+//MULTILÍNEA =Ya no es necesario poner /n
+/* let phrase2 = `Esta frase es una de las pruebas
+para probas las nuevas funciones de ECMAScript6`;
+console.log(phrase2); */
+//DESTRUCTURAR:
+/* let person = {
+    name: "Kader",
+    age: 25,
+    country: "Turquía",
+}
+let {name, age, country} = person; */
+/* console.log(`Hola soy ${name}, y soy ${country}.`); */
+// console.log(name, age, country); 
+// antes console.log(person.name);
+
+//Operador de Propagación = ...:
+/* let team1 = [`Luisa`, `Kader`, `Alma`];
+let team2 = [`Chris`, `Mauricio`, `Rodrigo`];
+let teamFinal = [`David`, ...team1, ...team2];
+console.log(teamFinal); */
+
+//LET
+//variable local = scope = bloque de código.
+/* function amigos(person = "Mauricio") {
+    let amigo = {
+        name: "Leo",
+        sexo: "Masculino",
+    }
+    console.log(amigo);
+    console.log(person);
+} 
+amigos(); */
+
+//CONST = NO SE PUEDE MODIFICAR el valor.
+/* const a = `b`; */
+
+//ARROW FUNCTIONS, PROMESAS, PARÁMETROS EN OBJETOS.
+
+
 
