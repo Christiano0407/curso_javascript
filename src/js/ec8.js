b26d5d1d7abb70e1e597c4cffcf78b2f0bc8ccfc
Christiano0407 <placebo0704@gmail.com>
2020-10-25 15:50:37 -0600

    Async and Await

---
+++ src/js/ec8.js
@@ -0,0 +1,30 @@
+//ECS8
+//OBJECT.ENTRIES (Object.entries()) = dEVOLVER LA CLAVE Y VALORES DE UNA MATRIZ (array).
+
+/* const data = {
+    frontend: `Chris`,
+    backend: `Isabel`,
+    uxDesigner: `Samantha`,
+}
+
+const entries = Object.entries(data);
+console.log(entries);
+console.log(entries.length); */
+
+//Object.values = valores de un Object a un Array
+/* const data = {
+    frontend: `Chris`,
+    backend: `Isabel`,
+    uxDesigner: `Samantha`,
+    edito: `Natasha`,
+}
+const values = Object.values(data);
+console.log(values);
+console.log(values.length); */
+
+//PADDING-Estructura de elementos. 
+/* const string = `Hello`;
+console.log(string.padStart(7, `Di`));
+console.log(string.padEnd(8, `End`)); */
+
+//ASYNC AND AWAIT.
