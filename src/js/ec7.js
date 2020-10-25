a0c666dfa03d1b69141300131eb4848f0f2f9f48
Christiano0407 <placebo0704@gmail.com>
2020-10-25 15:27:25 -0600

    ECMAScripts

---
+++ src/js/ec7.js
@@ -0,0 +1,14 @@
+//ECMAScript7
+//método ".includes". Para saber si tenemos un valor dentro de string, array..
+/* let numbers = [1,2,3,4,5];
+if(numbers.includes(5)) {
+    console.log("Sí, tenemos un cinco");
+}else {
+    console.log("No, no lo tiene");
+} */
+
+//Elevar a la potencia (**).
+/* let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+console.log(result); */
