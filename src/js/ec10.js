fe3f1160a041e0fc9a00a11620522cfc9a3eeef0
Christiano0407 <placebo0704@gmail.com>
2020-10-26 08:59:04 -0600

    finished

---
+++ src/js/ec10.js
@@ -0,0 +1,28 @@
+//EC10
+//Método:flat (profundidad).
+/* let array = [1,2,3, [4,5,6, [7,8,9]]];
+console.log(array.flat(2)); */
+
+//flatMap(mapearmétodos.)
+/* let array2 = [1,2,3, [4,5,6, [7,8,9]]];
+console.log(array2.flatMap(value => [value, value * 2])); */
+
+//eliminar espacios en blanco de un "string" => trimStart():
+/* let hello = "         Hello";
+console.log(hello);
+console.log(hello.trimStart()); */
+//Eliminar espacios al final => trimEnd().
+
+//fromEntries => clave valor a objeto.
+//Object.fromEntries().
+//array a objetos
+
+/* let entries = [["name", "Chris"], ["age", 28]];
+console.log(Object.fromEntries(entries)); */
+
+//SYMBOL
+//Descripción de un objeto tipo símbolo
+
+/* let mySymbl = "HelloSymbol";
+let symbol = Symbol(mySymbl);
+console.log(symbol.description); */
