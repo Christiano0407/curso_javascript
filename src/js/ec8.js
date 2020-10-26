3cdbab16b32089be44af68820dd34c89f15552af
Christiano0407 <placebo0704@gmail.com>
2020-10-26 07:23:48 -0600

    Async and Wait-try and catch

--- src/js/ec8.js
+++ src/js/ec8.js
@@ -27,4 +27,49 @@ console.log(values.length); */
 console.log(string.padStart(7, `Di`));
 console.log(string.padEnd(8, `End`)); */
 
-//ASYNC AND AWAIT.
+/* ASYNC AND AWAIT. */
+//Asincronismo. 
+//ASYNC => Hace que la función devuelva una promesa.
+//AWAIT => Antes de una función hace que la función espere una promesa. / Sólo se puede usar dentro de una async function.
+//Try and Catch.
+
+//1)
+/* const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+         resolve(`Hello World`);
+        }else {
+         reject(`Test Error`);
+        }
+    })
+}
+helloWorld()
+.then(response => console.log(response))
+.catch(error => console.log(error))
+
+const helloAsync = async() => {
+    const hello = await helloWorld()
+    console.log(hello);
+};
+
+helloAsync(); */
+
+//2)
+/* const hola = (amigo = "Mauricio", novia = "Kader") => {
+    return new Promise ((resolve, reject) => {
+        if(amigo === "Mauricio" || novia === "Kader"){
+         resolve(`Son las personas que mas quiero en la vida`);
+        }else {
+         reject(`No son nada para mi.`)
+        }
+    })
+}
+hola()
+.then(response => console.log(response))
+.catch(error => console.log(error))
+
+const holaAsync = async() => {
+    const saludo = await hola()
+    console.log(saludo);
+};
+holaAsync(); */
